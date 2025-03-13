21a24941ad0b10ae1bb7877e235ae5f2e2e098de
brann <brann@192.168.1.133>
2025-03-13 13:52:59 -0600

    tienda CRUD

---
+++ Documents/Universidad/desarrollo web/Tienda_Ali_Maki/src/main/java/com/ali_maki/AlimakiApplication.java
@@ -0,0 +1,13 @@
+package com.ali_maki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimakiApplication.class, args);
+	}
+
+}
